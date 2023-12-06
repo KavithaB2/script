3d1c6058cb30b14470b7ad197e9fed7c1eb658d8
KavithaB2 <50858280+KavithaB2@users.noreply.github.com>
2023-12-06 18:38:20 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 #creating ec2 instance ec2.tf
 resource "aws_instance""instance1"{
 ami="ami-0d92749d46e71c34c"
-instance_type="t2.micro"
+instance_type="t2.small"
 key_name="forssh"
 vpc_security_group_ids=["${aws_security_group.demosg.id}"]
 subnet_id=aws_subnet.public-subnet-1.id
